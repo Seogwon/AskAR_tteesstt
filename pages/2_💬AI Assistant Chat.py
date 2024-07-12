2c98dde999b72af22e396e985d36ba9c2d11dc5f
CSL-IBM <changsup.lim1@ibm.com>
2024-07-12 22:11:39 +0900

    Update 2_💬AI Assistant Chat.py

--- pages/2_💬AI Assistant Chat.py
+++ pages/2_💬AI Assistant Chat.py
@@ -31,7 +31,6 @@ llm = WatsonxLLM(LLAMA2_model)
 @st.cache(allow_output_mutation=True, hash_funcs={sqlite3.Connection: id})
 def get_db_connection():
     conn = sqlite3.connect('history.db', check_same_thread=False)
-    conn.row_factory = sqlite3.Row
     return conn
 
 # Initialize Streamlit app

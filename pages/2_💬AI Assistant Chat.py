558b5c5d20b6e3bc81f2b5d32583acb4b2b3cd8e
CSL-IBM <changsup.lim1@ibm.com>
2024-07-13 00:34:43 +0900

    Update 2_💬AI Assistant Chat.py

--- pages/2_💬AI Assistant Chat.py
+++ pages/2_💬AI Assistant Chat.py
@@ -163,6 +163,19 @@ def main():
         """,
         unsafe_allow_html=True
     )
+     
+    st.markdown(
+    """
+    <style>
+    table.dataframe {
+        width: 100%;
+        table-layout: auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
 if __name__ == '__main__':
     main()

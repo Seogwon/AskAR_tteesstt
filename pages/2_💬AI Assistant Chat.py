91dc88872e7e4cd7a49fd796cd505aa4dc5cb237
CSL-IBM <changsup.lim1@ibm.com>
2024-07-12 23:51:19 +0900

    Update 2_💬AI Assistant Chat.py

--- pages/2_💬AI Assistant Chat.py
+++ pages/2_💬AI Assistant Chat.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import sqlite3
 import csv
-from ibm_watson_machine_learning import APIClient
 from ibm_watson_machine_learning.foundation_models import Model
 from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
 from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
